0e4ffe34bac8844e8023123e5a3b636dc417b6fa
Hua Zhou <huazhou@ucla.edu>
2018-11-30 11:01:42 -0800

    try to implement Fisher information matrix; still fails to pass the test

--- src/polrfit.jl
+++ src/polrfit.jl
@@ -24,7 +24,7 @@ function polrfun!(
     
     T = eltype(m.X)
     needgrad && fill!(m.∇, 0)
-    needhess && fill!(m.H, 0)
+    needhess && fill!(m.FIM, 0)
     # update η according to X and β
     mul!(m.η, m.X, m.β)
     # update θ according to α
@@ -36,20 +36,20 @@ function polrfun!(
         m.θ[j] = m.θ[j-1] + exp(m.α[j])
         m.dθdα[j, j:end] .= exp(m.α[j])
     end
-    minζ, maxζ = convert(T, -100), convert(T, 100)
+    minη, maxη = convert(T, -100), convert(T, 100)
     logl = zero(T)
     for obs in 1:m.n
         wtobs = isempty(m.wts) ? one(T) : m.wts[obs]
         wtobs == 0 && continue
         ### log-likelihood
         yobs = m.Y[obs]
-        ζ1 = yobs == m.J ? maxζ : m.θ[yobs]   - m.η[obs]
-        ζ2 = yobs == 1   ? minζ : m.θ[yobs-1] - m.η[obs]
-        ζ1 = clamp(ζ1, minζ, maxζ) # for numerical stability
-        ζ2 = clamp(ζ2, minζ, maxζ)
-        cumprob1 = GLM.linkinv(m.link, ζ1)
-        cumprob2 = GLM.linkinv(m.link, ζ2)
-        py = cumprob1 - cumprob2
+        ηy   = yobs == m.J ? maxη : m.θ[yobs]   - m.η[obs]
+        ηym1 = yobs == 1   ? minη : m.θ[yobs-1] - m.η[obs]
+        ηy   = clamp(ηy  , minη, maxη) # for numerical stability
+        ηym1 = clamp(ηym1, minη, maxη) # ym1 reads y-minus-one
+        cumproby   = GLM.linkinv(m.link, ηy)
+        cumprobym1 = GLM.linkinv(m.link, ηym1)
+        py = cumproby - cumprobym1
         logl += py ≤ 0 ? typemin(T) : wtobs * log(py)
         ### gradient
         needgrad || continue
@@ -61,29 +61,49 @@ function polrfun!(
             m.wt∂β[obs] = zero(T)
         else
             # derivative of θ
-            pyinv  = inv(py)
-            deriv1 = pyinv * GLM.mueta(m.link, ζ1)
-            deriv2 = pyinv * GLM.mueta(m.link, ζ2)
-            derivΔ = deriv1 - deriv2
-            yobs < m.J && (m.∇[yobs  ] += wtobs * deriv1)
-            yobs > 1   && (m.∇[yobs-1] -= wtobs * deriv2)
+            pyinv    = inv(py)
+            derivy   = GLM.mueta(m.link, ηy)
+            derivym1 = GLM.mueta(m.link, ηym1)
+            derivΔ = derivy - derivym1
+            yobs < m.J && (m.∇[yobs  ] += wtobs * pyinv * derivy)
+            yobs > 1   && (m.∇[yobs-1] -= wtobs * pyinv * derivym1)
             # derivative of β = wt[obs] * X[:. obs]
-            m.wt∂β[obs] = - derivΔ
+            m.wt∂β[obs] = - pyinv * derivΔ
         end
         ### hessian
         needhess || continue
-        if py > 0
-            h1 = pyinv * muetad2(m.link, ζ1)
-            h2 = pyinv * muetad2(m.link, ζ2)
-            # hessian for ∂θ^2
-            if     yobs > 1  ; m.H[yobs-1, yobs-1] -= wtobs * (h2 + deriv2 * deriv2); end
-            if 1 < yobs < m.J; m.H[yobs  , yobs-1] += wtobs * (deriv1 * deriv2); end
-            if     yobs < m.J; m.H[yobs  , yobs  ] += wtobs * (h1 - deriv1 * deriv1); end
-            # hessian for ∂θ∂β
-            yobs > 1   && (m.wt∂θ∂β[obs, yobs - 1] += h2 - deriv2 * derivΔ)
-            yobs < m.J && (m.wt∂θ∂β[obs, yobs    ] -= h1 - deriv1 * derivΔ)
-            # hessian for ∂β∂β
-            m.wt∂β∂β[obs] += h1 - h2 - derivΔ * derivΔ
+        ηjm1 = ηj = ηjp1 = zero(T)
+        cumprobjm1 = cumprobj = cumprobjp1 = zero(T)
+        derivjm1 = derivj = derivjp1 = zero(T)
+        for j in 1:m.J-1
+            # systematic components at j-1, j, j+1
+            ηjm1       = j == 1 ? minη : ηj
+            ηj         = j == 1 ? m.θ[1] - m.η[obs] : ηjp1
+            ηjp1       = j == m.J-1 ? maxη : m.θ[j+1] - m.η[obs]
+            # cumulative probabilities at j-1, j, j+1
+            cumprobjm1 = j == 1 ? zero(T) : cumprobj
+            cumprobj   = j == 1 ? GLM.linkinv(m.link, ηj) : cumprobjp1
+            cumprobjp1 = j == m.J-1 ? one(T) : GLM.linkinv(m.link, ηjp1)
+            # point mass at j-1, j, j+1
+            pj         = cumprobj - cumprobjm1
+            pjp1       = cumprobjp1 - cumprobj
+            pjinv      =   pj ≤ 0 ? zero(T) : inv(pj)
+            pjp1inv    = pjp1 ≤ 0 ? zero(T) : inv(pjp1)
+            # mueta at j-1, j, j+1
+            derivjm1   = j == 1 ? zero(T) : derivj
+            derivj     = j == 1 ? GLM.mueta(m.link, ηj) : derivjp1
+            derivjp1   = j == m.J-1 ? zero(T) : GLM.mueta(m.link, ηjp1)
+            # FIM for ∂θ^2
+            m.FIM[j, j] += wtobs * (pjinv + pjp1inv) * derivj * derivj
+            obs == m.n && (@show m.FIM[j, j])
+            if j > 1
+                m.FIM[j, j-1] -= wtobs * pjinv * derivj * derivjm1
+            end
+            # FIM for ∂θ∂β, weights only
+            m.wt∂θ∂β[obs, j] += (pjinv * derivjm1 - (pjinv + pjp1inv) * derivj +
+                pjp1inv * derivjp1) * derivj
+            # FIM for ∂β∂β, weights only
+            m.wt∂β∂β[obs] += pjinv * (derivj - derivjm1)^2
         end
     end
     if needgrad
@@ -96,20 +116,24 @@ function polrfun!(
     end
     if needhess
         if isempty(m.wts)
-            @views mul!(m.H[m.J:end, 1:m.J-1], transpose(m.X), m.wt∂θ∂β)
+            # ∂θ∂β
+            @views mul!(m.FIM[m.J:end, 1:m.J-1], transpose(m.X), m.wt∂θ∂β)
+            # ∂β∂β
             copyto!(m.scratchm1, m.X)
             lmul!(Diagonal(m.wt∂β∂β), m.scratchm1)
-            @views mul!(m.H[m.J:end, m.J:end], transpose(m.X), m.scratchm1)
+            @views mul!(m.FIM[m.J:end, m.J:end], transpose(m.X), m.scratchm1)
         else
+            # ∂θ∂β
             copyto!(m.scratchm1, m.X)
             lmul!(Diagonal(m.wts), m.scratchm1)
-            @views mul!(m.H[m.J:end, 1:m.J-1], transpose(m.scratchm1), m.wt∂θ∂β)
+            @views mul!(m.FIM[m.J:end, 1:m.J-1], transpose(m.scratchm1), m.wt∂θ∂β)
+            # ∂β∂β
             m.wtwk .= m.wts .* m.wt∂β∂β
             copyto!(m.scratchm1, m.X)
             lmul!(Diagonal(m.wtwk), m.scratchm1)
-            @views mul!(m.H[m.J:end, m.J:end], transpose(m.X), m.scratchm1)
+            @views mul!(m.FIM[m.J:end, m.J:end], transpose(m.X), m.scratchm1)
         end
-        LinearAlgebra.copytri!(m.H, 'L')
+        LinearAlgebra.copytri!(m.FIM, 'L') # copy lower triangular to upper triangular
     end
     logl
 end
@@ -181,10 +205,13 @@ function fit(
     copyto!(dd.α, 1, xsol, 1, dd.J - 1)
     copyto!(dd.β, 1, xsol, dd.J, dd.p)
     polrfun!(dd, true, true)
-    Hbk = bunchkaufman!(Symmetric(dd.H), check=false)
-    if issuccess(Hbk)
-        dd.vcov[:] = -inv(Hbk)
-    else # Hessian is singular
+    @show dd.FIM[1:2, 1:2]
+    @show dd.FIM
+    @show eigvals(Symmetric(dd.FIM))
+    FIMbk = bunchkaufman(Symmetric(dd.FIM), check=false)
+    if issuccess(FIMbk)
+        dd.vcov[:] = inv(FIMbk)
+    else # FIM is singular
         dd.vcov .= Inf
     end
     return dd
@@ -214,39 +241,3 @@ function MathProgBase.eval_grad_f(m::PolrModel, grad::Vector, par::Vector)
     @views mul!(grad[1:m.J-1], m.dθdα, m.∇[1:m.J-1])
     copyto!(grad, m.J, m.∇, m.J, m.p)
 end
-
-"""
-muetad2(::CuachitLink, η)
-
-`d^2μ/dη^2` for CauchitLink.
-"""
-muetad2(::CauchitLink, η) = - 2η / (pi * (one(η) + abs2(η)))^2
-
-"""
-muetad2(::CloglogLink, η)
-
-`d^2μ/dη^2` for CloglogLink.
-"""
-function muetad2(::CloglogLink, η::T) where T<:Real
-    expη = exp(η)
-    expη * exp(-expη) * (one(η) - expη)
-end
-
-"""
-muetad2(::LogitLink, η)
-
-`d^2μ/dη^2` for LogitLink.
-"""
-function muetad2(::LogitLink, η)
-    expabs = exp(-abs(η))
-    denominv = inv(1 + expabs)
-    term1 = expabs * denominv * denominv
-    η ≥ 0 ? term1 * (1 - 2denominv) : term1 * (1 - 2expabs * denominv)
-end
-
-"""
-muetad2(::ProbitLink, η)
-
-`d^2μ/dη^2` for ProbitLink.
-"""
-muetad2(::ProbitLink, η) = - (exp(-abs2(η) / 2) / GLM.sqrt2π) * η
