318b4bd2e12de7dd0353e6597e6925284498b8a0
Hua Zhou <huazhou@ucla.edu>
2019-02-11 19:11:47 -0800

    update pkg name

--- docs/make.jl
+++ docs/make.jl
@@ -1,14 +1,14 @@
-using Documenter, PolrModels
+using Documenter, OrdinalMultinomialModels
 
 ENV["DOCUMENTER_DEBUG"] = "true"
 
 makedocs(
     format = :html,
-    sitename = "PolrModels",
+    sitename = "OrdinalMultinomialModels",
     modules = [PolrModels]
 )
 
 deploydocs(
-    repo   = "github.com/OpenMendel/PolrModels.jl.git",
+    repo   = "github.com/OpenMendel/OrdinalMultinomialModels.jl.git",
     target = "build"
 )
